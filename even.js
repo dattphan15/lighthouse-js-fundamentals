c73e753661cc57de7413de8d591d1636bcd0f4af
Kevin Phan <ken.phan@live.ca>
2020-12-13 23:23:33 +0000

    Checking if values are even

---
+++ even.js
@@ -0,0 +1,20 @@
+/*
+const isEven  = function (num) {
+  return num % 2 === 0;
+}
+
+const tenIsEven = isEven(10);
+const elevenIsEven = isEven(11);
+
+console.log(tenIsEven);
+console.log(elevenIsEven);
+*/
+
+// the function below is more concise and does the same thing as the function above
+
+const isEven  = function (num) {
+  return num % 2 === 0;
+}
+
+console.log(isEven(10));
+console.log(isEven(11));
